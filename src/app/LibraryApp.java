8be80bbe447eb83d4e4149ca85f63657aecb2387
Bartłomiej Kubica <Bartomiej Kubica>
2017-02-13 21:41:11 +0100

    Upload Library project v.0.7

---
+++ src/app/LibraryApp.java
@@ -0,0 +1,15 @@
+package app;
+
+public class LibraryApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		final String appName = "Biblioteka v 0.7";
+		
+		System.out.println(appName);
+		LibraryControl libControl = new LibraryControl();
+		libControl.controlLoop();
+	}
+
+}
