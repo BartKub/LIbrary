b90f0c9de7011a283d3bb50a0a224f3a504b443d
Bartłomiej Kubica <Bartomiej Kubica>
2017-02-19 20:52:30 +0100

    version changed

--- src/app/LibraryApp.java
+++ src/app/LibraryApp.java
@@ -5,7 +5,7 @@ public class LibraryApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		final String appName = "Biblioteka v 0.8";
+		final String appName = "Biblioteka v 0.9";
 		
 		System.out.println(appName);
 		LibraryControl libControl = new LibraryControl();
