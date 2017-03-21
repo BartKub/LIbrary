0f29c8e5f0b2a161cef6585af43f3f644aa43ac8
Bartłomiej Kubica <Bartomiej Kubica>
2017-03-21 21:19:02 +0100

    enums created

--- src/app/LibraryControl.java
+++ src/app/LibraryControl.java
@@ -6,13 +6,7 @@ import data.Library;
 import data.Magazine;
  
 public class LibraryControl {
-    // zmienne do kontrolowania programu
-	 	public static final int EXIT = 0;
-	    public static final int ADD_BOOK = 1;
-	    public static final int ADD_MAGAZINE = 2;
-	    public static final int PRINT_BOOKS = 3;
-	    public static final int PRINT_MAGAZINES = 4;
- 
+   
     // zmienna do komunikacji z u¿ytkownikiem
     private DataReader dataReader;
  
@@ -28,9 +22,9 @@ public class LibraryControl {
      * G³ówna pêtla programu, która pozwala na wybór opcji i interakcjê
      */
     public void controlLoop() {
-        int option;
+        Option  option;
         printOptions();
-        while ((option = dataReader.getInt()) != EXIT) {
+        while ((option = Option.createFromInt(dataReader.getInt())) != Option.EXIT) {
             switch (option) {
             case ADD_BOOK:
                 addBook();
@@ -55,11 +49,9 @@ public class LibraryControl {
  
     private void printOptions() {
         System.out.println("Wybierz opcjê: ");
-        System.out.println(EXIT + " - wyjœcie z programu");
-        System.out.println(ADD_BOOK + " - dodanie nowej ksi¹¿ki");
-        System.out.println(ADD_MAGAZINE + " - dodanie nowego magazynu");
-        System.out.println(PRINT_BOOKS + " - wyœwietl dostêpne ksi¹¿ki");
-        System.out.println(PRINT_MAGAZINES + " - wyœwietl dostêpne magazyny");
+       for (Option o: Option.values()){
+    	   System.out.println(o);
+       }
     }
  
     private void addBook() {
---
+++ src/app/Option.java
@@ -0,0 +1,40 @@
+package app;
+
+public enum Option {
+	
+	EXIT(0, "Wyjscie z programu"),
+	ADD_BOOK(1, "Dodanie ksiazki"),
+	ADD_MAGAZINE(2, "Dodanie magazynu/gazety"),
+	PRINT_BOOKS(3, "Wyswietlenie dostepnych ksiazek"),
+	PRINT_MAGAZINES(4,"Wyswietlenie dostepnych magazymow");
+
+	private int value;
+	private String description;
+	
+	private Option(int value, String description) {
+		this.value = value;
+		this.description = description;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@Override
+	public
+	String toString(){
+		return value + " - "+ description;
+	}
+	
+	public static Option createFromInt(int option){
+		return Option.values()[option];
+	}
+}
