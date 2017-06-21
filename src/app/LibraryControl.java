8add1a36e8c420cd073529b22050a5235500aca5
Kubica <bartlomiej.kubica@solarwinds.com>
2017-06-21 13:53:00 +0200

    Inner Class Added, option class deleted

--- src/app/LibraryControl.java
+++ src/app/LibraryControl.java
@@ -2,6 +2,9 @@ package app;
  
 import utils.DataReader;
 import utils.LibraryUtils;
+
+import java.util.NoSuchElementException;
+
 import data.Book;
 import data.Library;
 import data.Magazine;
@@ -72,4 +75,36 @@ public class LibraryControl {
     private void printMagazines() {
         LibraryUtils.printMagazines(library);
     }
+    private enum Option {
+        EXIT(0, "Wyjście z programu"),
+        ADD_BOOK(1, "Dodanie książki"),
+        ADD_MAGAZINE(2,"Dodanie magazynu/gazety"),
+        PRINT_BOOKS(3, "Wyświetlenie dostępnych książek"),
+        PRINT_MAGAZINES(4, "Wyświetlenie dostępnych magazynów/gazet");
+      
+        private int value;
+        private String description;
+      
+        Option(int value, String desc) {
+            this.value = value;
+            this.description = desc;
+        }
+          
+        @Override
+        public String toString() {
+            return value + " - " + description;
+        }
+          
+        public static Option createFromInt(int option) throws NoSuchElementException {
+            Option result = null;
+            try {
+                result = Option.values()[option];
+            } catch(ArrayIndexOutOfBoundsException e) {
+                throw new NoSuchElementException("Brak elementu o wskazanym ID");
+            }
+              
+            return result;
+        }
+    }
+
 }
